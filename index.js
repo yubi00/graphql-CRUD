3827150b9006b20735800434200bd22855a006cf
yubi <ubrajkhadka@gmail.com>
2021-06-14 11:57:05 +1000

    graphql playground flow

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-//serve static assets
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
 server.applyMiddleware({ app });
+
+//serve static assets
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on ${port}`));
